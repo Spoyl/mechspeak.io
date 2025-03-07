05ec06725d8fd00708c30a6472763730592ed142
oli stocks <stocksolly@gmail.com>
2025-03-07 18:58:38 +0800

    :poop: clean up last commit

--- services/calendar/main.py
+++ services/calendar/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from datetime import date
 from models import Lesson, Timezones
 from typing import Annotated
@@ -9,12 +9,25 @@ CURRENT_DATE = date.isoformat(date.today())
 app = FastAPI()
 
 
-@app.get("/calendar/")
-def read_day_itinerary(date: Annotated[str | None, Query(title="Requested Date", pattern = "^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$")] = CURRENT_DATE, 
-                       tmz: Timezones | None = Timezones.GMT):
+@app.get("/lessons/")
+def read_day_itinerary(
+    date: Annotated[str | None, Query(title="Requested Date", pattern = r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$")] = CURRENT_DATE, 
+    tmz: Annotated[Timezones | None, Path(title = "The display timezone for the calendar")] = Timezones.GMT):
     return {"date":date, "tmz":tmz}
 
-@app.post("/booking/")
+@app.post("/lessons/")
 async def make_booking(booking: Lesson):
     Lesson.lesson_id = Lesson.dat + "_" + Lesson.start + "_" + Lesson.dur
     return booking
+
+@app.get("lessons/{lesson_id}")
+async def lesson_info():
+    return None
+
+@app.put("lessons/{lesson_id}")
+async def update_info():
+    return None
+
+@app.delete("lessons/{lesson_id}")
+async def cancel():
+    return None
