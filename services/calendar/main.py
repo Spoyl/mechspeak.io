05a4bec0e5383657a5039cfecec954d42d7b3549
oli stocks <stocksolly@gmail.com>
2025-03-20 08:03:31 +0700

    :construction: on pause

--- services/calendar/main.py
+++ services/calendar/main.py
@@ -1,33 +1,79 @@
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, status, Depends
 from datetime import date
-from models import Lesson, Timezones
+from models import Lesson, Timezones, Booking
 from typing import Annotated
+from sqlmodel import create_engine, select, SQLModel, Session
 
 
 CURRENT_DATE = date.isoformat(date.today())
+
+# db params:
+sqlite_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tabulate():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session())]
+
 
 app = FastAPI()
 
 
 @app.get("/lessons/")
-def read_day_itinerary(
-    date: Annotated[str | None, Query(title="Requested Date", pattern = r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$")] = CURRENT_DATE, 
-    tmz: Annotated[Timezones | None, Path(title = "The display timezone for the calendar")] = Timezones.GMT):
+def request_itin(
+    date: Annotated[str | None, Query(pattern = r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$")] = CURRENT_DATE, 
+    tmz: Timezones | None = Timezones.GMT):
+    """
+    Request method. Retrieves schedule information from the db. 
+    """
     return {"date":date, "tmz":tmz}
 
-@app.post("/lessons/")
+
+@app.post("/lessons/", status_code=status.HTTP_201_CREATED)
 async def make_booking(booking: Lesson):
-    Lesson.lesson_id = Lesson.dat + "_" + Lesson.start + "_" + Lesson.dur
-    return booking
+    """
+    Post method. Creates a new entry in the calendar db.
+    """
+    lesson_dict = booking.model_dump()
+    Lesson.lesson_id = lesson_dict["dat"] + "_" + lesson_dict["start"] + "_" + str(lesson_dict["dur"])
+    return {"lesson_id": Lesson.lesson_id}
 
-@app.get("lessons/{lesson_id}")
-async def lesson_info():
-    return None
 
-@app.put("lessons/{lesson_id}")
-async def update_info():
-    return None
+@app.get("/lessons/{lesson_id}")
+async def lesson_info(lesson_id: Annotated[str, Path(title = "Unique lesson identifier")]):
+    """
+    Request method. Retrieves information about a specific lesson from the server.
+    """
+    results = {"lesson_id" : lesson_id}
+    return results
 
-@app.delete("lessons/{lesson_id}")
-async def cancel():
+
+@app.put("/lessons/{lesson_id}")
+async def update_info(lesson_id: Annotated[str, Path(title = "Unique lesson identifier")],
+                      reschedule: bool | None = None,
+                      cancel: bool | None = None):
+    """
+    Method for updating an event in the calendar.
+    """
+    if cancel:
+        result = "Cancel Lesson " + str(lesson_id)
+    else:
+        result = reschedule.model_dump()
+
+    return result
+
+
+@app.delete("/lessons/{lesson_id}")
+async def cancel(lesson_id: Annotated[str, Path(title = "Unique lesson identifier")]):
+    """
+    Method to delete an event in the calendar.
+    """
     return None
--- services/calendar/models.py
+++ services/calendar/models.py
@@ -1,19 +1,44 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from enum import Enum
-from datetime import date
+from uuid import UUID
+from datetime import datetime
+from sqlmodel import SQLModel, Field
 
+
+class Booking(SQLModel, table=True):
+    lesson_id: UUID | None = Field(primary_key=True, default=None)
+    start: datetime = Field(index=True, examples=["2025-03-09T07:28:12.955Z"])
+    dur: int = Field(default= 50, description="A value of '50' is a lesson, '25' is a trial")
+    paid: bool
+    who: list[str] = set()
 
 
 class Timezones(str, Enum):
-    GMT = "GMT"
-    Bali = "Bali"
-    France = "France"
+    GMT= "GMT"
+    Bali= "Bali"
+    France= "France"
 
 
 class Lesson(BaseModel):
-    dat: str
-    start: str
-    dur: int
+    lesson_id: UUID
+    start: datetime = Field(examples=["2025-03-09T07:28:12.955Z"])
+    dur: int = Field(default= 50, description="Duration defines whether the lesson is a trial or not")
     paid: bool
-    who: str | None = None
-    lesson_id: str
+    who: list[str] = set()
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "lesson_id": "550e8400-e29b-41d4-a716-446655440000",
+                    "description": "2025-03-09T07:28:12.955Z",
+                    "dur": 25,
+                    "paid": True,
+                    "who": [
+                        "foo@bar.com",
+                        "example_user@email.com"
+                    ]
+                }
+            ]
+        }
+    }
